d5fb850e2c3ac250bc69bc58b3d1bb0389818576
diego-torres <diego.torres.fuerte@gmail.com>
2015-12-10 12:33:30 -0700

    first draft version

---
+++ CA2/website/app/scripts/controllers/products.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ca2App.controller:ProductsCtrl
+ * @description
+ * # ProductsCtrl
+ * Controller of the ca2App
+ */
+angular.module('ca2App')
+  .controller('ProductsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ CA2/website/app/scripts/controllers/products_switches.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ca2App.controller:ProductsSwitchesCtrl
+ * @description
+ * # ProductsSwitchesCtrl
+ * Controller of the ca2App
+ */
+angular.module('ca2App')
+  .controller('ProductsSwitchesCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
